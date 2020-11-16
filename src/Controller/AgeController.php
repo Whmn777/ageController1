f4a80e4eaf7cb190887c0f9a58e6379a6caf3981
Whmn777 <helene.hnawia@lapiscine.pro>
2020-11-16 19:06:57 +0100

    class Request et Response pour récuperer nom et prénom

--- src/Controller/AgeController.php
+++ src/Controller/AgeController.php
@@ -15,7 +15,7 @@ class AgeController
 
         $age = $request->query->get('age');
 
-        if ($age < 18) {
+        if ($age >= 18) {
             echo "bienvenue";
         }
         else {
@@ -23,4 +23,30 @@ class AgeController
         }
 
     }
+
+
+    /*
+     * *Je crée une nouvelle route pour nom et prénom (je définis l'url, puis je le nomme avec name="".)
+     ** @Route("/nomPrenom", name="nomPrenom")
+     */
+
+    //J'utilise une méthode publique de Symfony  pour récupérer grâce à la classe Request le nom et le prénom dans l'url.
+
+    public function recuperer_nomPrenom (Request $request){
+
+        $nom = $request->query->get('nom');
+        $prenom = $request->query->get('prenom');
+
+        //Je crée une instance de la classe Responce contenant une chaîne de caractères et des balises HTML
+        //Je stocke la réponse dans une variable $response.
+
+        $response= newResponse('<p>Bonjour'.$nom." ".$prenom.'</p>');
+
+        //Je retourne ma méthode Request
+
+        return $response;
+
+    }
+
+
 }
