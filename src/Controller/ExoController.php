4fcd6f4f27fde69201bc995928c79aa6fd313e5f
Whmn777 <helene.hnawia@lapiscine.pro>
2020-11-18 15:31:37 +0100

    for twig et listes

--- src/Controller/ExoController.php
+++ src/Controller/ExoController.php
@@ -27,7 +27,7 @@ class ExoController extends AbstractController
                 "active" => true
             ];
 
-        //Je retourne ma réponse http en affichant sur le navigateur la page html profile.html.twig.twig
+        //Je retourne ma réponse http en affichant sur le navigateur la page html profile.html.twig
         //J'utilise la méthode render de la class AbstractController :
         //Je fais afficher une variable en utilisant la méthode render avec en paramètre un tableau contenant
         //cette même variable : ici $profile qui est un array
@@ -36,9 +36,43 @@ class ExoController extends AbstractController
 
         return $this->render('profile.html.twig',
             [
-            'profile'=>$profile
+            'profile'=>$profile //'profile'=nom de la variable dans twig=>qui prend pour valeur la variable $profile
             ]);
 
+        //Dans la méthode render, Symfony va compiler le fichier twig en html afin qu'il soit lisible par le navigateur
+        //les variables en php seront aussi affichées, avec la page html, avec leurs valeurs).
+    }
+
+    /**
+     *  Je crée une page avec une nouvelle route pour récuperer les skills du profil utilisateur,
+     * @Route("/skills", name="skills")
+     */
+
+
+    public function getSkills()
+    {
+
+        $skills =
+            ["serviable",
+                "aimable",
+                "drôle",
+                "poli",
+            ];
+
+        //Je retourne ma réponse http en affichant sur le navigateur la page html profile.html.twig
+        //J'utilise la méthode render de la class AbstractController :
+        //Je fais afficher une variable en utilisant la méthode render avec en paramètre un tableau contenant
+        //cette même variable : ici $skills qui est un array
+        //c'est à dire :
+
+
+        return $this->render('skills.html.twig',
+            [
+                'skills'=>$skills //'skills'=nom de la variable dans twig=>qui prend pour valeur la variable $skills
+            ]);
+
+        //Dans la méthode render, Symfony va compiler le fichier twig en html afin qu'il soit lisible par le navigateur
+        //les variables en php seront aussi affichées, avec la page html, avec leurs valeurs).
     }
 
 }
